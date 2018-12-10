d8d96cf8ec4ca3b1c7f2a5b5e47e459ab3935a27
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-12-10 22:07:28 +0000

    Created page three (html, css and js files)

---
+++ js/page3.js
@@ -0,0 +1,36 @@
+$(function() {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+$("#img1").click(function() {
+  $("#textmercury").fadeToggle("fast");
+});
+
+
+$("#img2").click(function() {
+  $("#textvenus").fadeToggle("fast");
+});
+
+$("#img3").click(function() {
+  $("#textearth").fadeToggle("fast");
+});
+
+$("#img4").click(function() {
+  $("#textmars").fadeToggle("fast");
+});
+
+$("#img5").click(function() {
+  $("#textjupiter").fadeToggle("fast");
+});
+
+$("#img6").click(function() {
+  $("#textsaturn").fadeToggle("fast");
+});
+
+$("#img7").click(function() {
+  $("#texturanus").fadeToggle("fast");
+});
+
+$("#img8").click(function() {
+  $("#textneptune").fadeToggle("fast");
+});
